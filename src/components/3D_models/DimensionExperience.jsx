73293e244f10e73185c5c0b741cc9660fc5a2a87
naymaaminnishy <naymaaminnishy06@gmail.com>
2025-10-22 01:54:57 +0600

    Add all recent changes

--- src/components/3D_models/DimensionExperience.jsx
+++ src/components/3D_models/DimensionExperience.jsx
@@ -1,38 +1,43 @@
-import { OrbitControls } from "@react-three/drei"
-import { Canvas } from "@react-three/fiber"
+import { OrbitControls, useTexture } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { useMediaQuery } from "react-responsive";
-import { Bedroom } from "./Bedroom";
-import DimensionLights from "./DimensionLights";
+import * as THREE from "three";
 import Snowy from "./Snowy";
 
-const DimensionExperience = () => {
-  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
-  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+const ImageModel = () => {
+  const texture = useTexture("/images/nishy_blue.jpg");
   return (
-    <Canvas camera={{ position: [0, 0, 15], fov: 45 }}>
+    <mesh position={[0, 0, 0]}>
+      <planeGeometry args={[4, 6]} />
+      <meshBasicMaterial map={texture} side={THREE.DoubleSide} transparent />
+     <mesh>
+      <planeGeometry args={[4.5, 6.5]} />
+       <meshBasicMaterial color="#45DEC4" wireframe />
+     </mesh>
+    </mesh>
+  );
+};
 
+const DimensionExperience = () => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
+  return (
+    <Canvas camera={{ position: [0, 0, 10], fov: 45 }}>
       <OrbitControls
         enablePan={false}
-        enableZoom={!isTablet}
-        maxDistance={15}
-        minDistance={5}
+        enableZoom={false}
+        enableRotate={true}
         maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 5}
+        minPolarAngle={Math.PI / 2}
+        target={[0, 0, 0]}
       />
-      <DimensionLights />
 
       <Snowy count={200} />
-      <group
-        scale={isMobile ? 0.5 : 1}
-        position={[0, -3.5, 0]}
-        rotation={[0, -Math.PI / 4, 0]}
-      >
-
-        <Bedroom />
+      <group scale={isMobile ? 1 : 1} position={[0, 0, 0]}>
+        <ImageModel />
       </group>
-
     </Canvas>
-  )
-}
+  );
+};
 
 export default DimensionExperience
